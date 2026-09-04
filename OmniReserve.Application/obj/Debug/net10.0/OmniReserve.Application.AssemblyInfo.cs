502f4f053283a462539c85d8ee5b480734871856
Fernando_Tintore <tintoref@gmail.com>
2026-09-04 11:41:49 -0600

    Intento_Tarea_2

--- OmniReserve.Application/obj/Debug/net10.0/OmniReserve.Application.AssemblyInfo.cs
+++ OmniReserve.Application/obj/Debug/net10.0/OmniReserve.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OmniReserve.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4eca9bc203d98dda8554962c58a59b8f6d01f1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c541a38abbb537a45aac681497bbd9214a02cf5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OmniReserve.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OmniReserve.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OmniReserve.api/Program.cs
+++ OmniReserve.api/Program.cs
@@ -1,12 +1,16 @@
+using OmniReserve.Application;
+using OmniReserve.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 
 if (app.Environment.IsDevelopment())
 {
